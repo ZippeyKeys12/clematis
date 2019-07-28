a5e5ec1db01af15604f9fca94b0ef61473a3bf50
ZippeyKeys12 <ZippeyKeys12@gmail.com>
2019-07-28 15:40:55 -0500

    Remove bobby

--- tools/test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import os
-from subprocess import run
-
-run(["py", "tools/bobby/build.py", "Clematis", "-C", "-V3.3"])
-print("Running Steam")
-run([r"C:\Program Files (x86)\Steam\Steam.exe", "-applaunch", "2300"])
